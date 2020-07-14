d24eebdaa3364acaee094cedb39d8b50b04a5cfa
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2020-07-14 17:15:44 +0200

    edit 03

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,11 @@ function searchProducts() {
   let x = document.querySelectorAll('.card'); 
     
   for (i = 0; i < x.length; i++) {  
-      if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+      if (!x[i].textContent.toLowerCase().includes(input)) { 
           x[i].style.display="none"; 
       } 
       else { 
-          x[i].style.display="flex";                  
+          x[i].style.display="inline-block";                  
       } 
   } 
 } 
