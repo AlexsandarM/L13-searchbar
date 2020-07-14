7a177c4817267904903aadcd8419e2a887b7cd6c
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2020-07-14 16:24:01 +0200

    edit 02

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Search logic
+
+function searchProducts() { 
+  let input = document.getElementById('search-input').value 
+  input=input.toLowerCase(); 
+  let x = document.querySelectorAll('.card'); 
+    
+  for (i = 0; i < x.length; i++) {  
+      if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+          x[i].style.display="none"; 
+      } 
+      else { 
+          x[i].style.display="flex";                  
+      } 
+  } 
+} 
+
+// let x = document.querySelectorAll('.card .card-text');
+// console.log(x)
