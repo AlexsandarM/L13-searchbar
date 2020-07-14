b26eba1c98ef5a3ba10ff9ecb0c3fe8fb12fab6f
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2020-07-14 17:46:22 +0200

    edit 04

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
 // Search logic
 
-function searchProducts() { 
-  let input = document.getElementById('search-input').value 
-  input=input.toLowerCase(); 
-  let x = document.querySelectorAll('.card'); 
-    
-  for (i = 0; i < x.length; i++) {  
-      if (!x[i].textContent.toLowerCase().includes(input)) { 
-          x[i].style.display="none"; 
-      } 
-      else { 
-          x[i].style.display="inline-block";                  
-      } 
-  } 
-} 
+function searchProducts() {
+    let input = document.getElementById('search-input').value
+    input = input.toLowerCase();
+    let x = document.querySelectorAll('.card');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].textContent.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "inline-block";
+        }
+    }
+}
 
 // let x = document.querySelectorAll('.card .card-text');
 // console.log(x)
